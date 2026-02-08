3f60a232b6f5a0aa01b32fe272f0ac1b35b1397e
masanisainikhil5-boop <masani.sainikhil5@gmail.com>
2026-02-09 00:04:37 +0530

    Playwright with Typescript
    Day 7 - Practicals on different types of functions like Named functions, Anonymous functions, Arrow functions with rest, optional, default and implicit parameters etc...

--- TSDemos/Day7/functions.ts
+++ TSDemos/Day7/functions.ts
@@ -32,3 +32,34 @@ function addnumbers(...nums:any[])
 }
 addnumbers(100,200)
 addnumbers(5,10,15)
+
+//Example3: Named function with rest parameters - multiple types
+function findelements(...elements:(number | string)[]):number 
+{
+    return elements.length
+}
+console.log("Number of elements in findelements:",findelements(3,"John",5,"Doe",10))
+console.log("Number of elements in findelements:",findelements(1,2,3,4,5))
+console.log("Number of elements in findelements:",findelements("Alice","Bob","Charlie"))
+
+//Example4: Named function with optional parameters
+function displaydetails(id:number, name:string, mailID?:string): void{ //? makes the parameter optional
+    console.log("ID:", id)
+    console.log("Name:", name)
+    if(mailID){
+        console.log("Mail ID:", mailID)
+    }
+}
+
+displaydetails(101,"John","john@gmail.com")
+displaydetails(102,"Alice")
+
+//Example5: Named function with default parameters
+function calculatediscount(price:number, rate:number=10):void{
+    let discount:number = price * rate / 100
+    console.log("Discount:", discount)
+}
+calculatediscount(638)
+calculatediscount(638,15)
+
+//This is an interview question. They ask about Named function with default, rest and optional parameters.
