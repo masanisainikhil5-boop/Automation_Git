5f545613d325c0947653fe6d972413c7df393c8d
masanisainikhil5-boop <masani.sainikhil5@gmail.com>
2026-02-11 22:27:20 +0530

    callback and overloaded functions

--- TSDemos/Day8/overloadedfunctions.ts
+++ TSDemos/Day8/overloadedfunctions.ts
@@ -19,3 +19,52 @@ function getinfo(value:number|string):string{
 console.log(getinfo(123)) // Output: ID: 123
 console.log(getinfo("John")) // Output: Name: John
 
+//Example 2: Different number of parameters
+function add(x:number, y:number):number
+function add(x:number, y:number, z:number):number
+
+function add(x:number, y:number, z?:number):number{
+    if(z!== undefined){
+        return x + y + z
+    }
+    return x + y
+}
+console.log(add(10,20)) // Output: 30
+console.log(add(10,20,30))
+
+//Example 3: Different return types
+function getValue(flag:string):string
+function getValue(flag:number):number
+
+function getValue(flag: string | number): string | number {
+    if(typeof flag ==="string"){
+        return flag.toUpperCase()
+    }
+    else{
+        return flag * 2
+    }
+}
+
+console.log(getValue("hello")) // Output: HELLO
+console.log(getValue(5)) // Output: 10
+
+//Example 4:
+function display(name:string):string
+function display(age:number):string
+function display(RichieRich:boolean):string
+
+function display(value: string | number | boolean):string{
+    if(typeof value === "string"){
+        return `Name: ${value}`
+    }
+    else if(typeof value === "number"){
+        return `Age: ${value}`
+    }
+    else{
+        return `RichieRich: ${value}`
+    }
+}
+
+console.log(display("Alice")) // Output: Name: Alice
+console.log(display(30)) // Output: Age: 30
+console.log(display(true)) // Output: RichieRich: true
