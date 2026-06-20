14b91971ab4ac8933b5301de893e9c67c63c9e48
masanisainikhil5-boop <masani.sainikhil5@gmail.com>
2026-06-20 23:28:47 +0530

    Update project structure and remove deprecated files

--- Python_programs/leapyear.py
+++
@@ -1,7 +0,0 @@
-year = 208
-
-if (year % 4==0 and year %100 != 0) or (year %400 == 0):
-    print(f"{year} is an leap year")
-else:
-    print(f"{year} is not an leap year")
-
--- Python_programs/starpattern.py
+++
@@ -1,7 +0,0 @@
-n = 5
-
-for i in range(1,n+1):
-    stars = ""
-    for j in range(1, i+1):
-        stars += "*"
-    print(stars)
